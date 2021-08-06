69e5f6e372498c21ae266e408ca37c4c66f6d9b7
Santio <santiothebest123@gmail.com>
2021-08-05 20:16:21 -0400

    Fix NSFW detection to also look for links

--- listeners/CartersCacti/imageFilter.ts
+++ listeners/CartersCacti/imageFilter.ts
@@ -1,10 +1,11 @@
 import { BotClient } from '../../utils/BotClient';
 import { Listener } from '../../utils/Listener';
-import { Message, MessageAttachment, MessageEmbed, TextChannel } from 'discord.js';
+import { Message, MessageAttachment, MessageEmbed, TextChannel, User } from 'discord.js';
 import fetch from 'node-fetch';
 
 export class imageFilter extends Listener {
   imageRegex: RegExp = RegExp(`(http)?s?:?(\/\/[^"']*\.(?:png|jpg|jpeg))`, 'i');
+  linkRegex: RegExp = /(\b(https?):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/gi;
 
   constructor(client: BotClient) {
     super(client, {
@@ -17,25 +18,29 @@ export class imageFilter extends Listener {
 
     client.on('message', async (message: Message) => {
       if (logChannel == null) logChannel = <TextChannel>await client.channels.fetch('861767697418944572');
-      if (message.guild?.id != '696027249002020896' || message.author.bot || logChannel == null || message.attachments.size == 0) return;
+      if (message.guild?.id != '696027249002020896' || message.author.bot || logChannel == null) return;
 
-      message.attachments.forEach(async (attachment: MessageAttachment) => {
-        if (this.imageRegex.test(attachment.url)) {
-          // Check if image is NSFW
-          var req = await fetch('https://api.santio.me/filter/image?url=' + attachment.url);
-          var data = await req.json();
-          if (data.flags.includes('Porn') || data.flags.includes('Hentai')) {
-            var embed: MessageEmbed = new MessageEmbed()
-              .setTitle(' ')
-              .setColor('#ff0000')
-              .setDescription(`Image was flagged as NSFW [Flags: ${data.flags.join(', ')}]\nPost by: **${message.author.tag}** *(${message.author.id})*`)
-              .setImage(attachment.url);
+      message.attachments.forEach((attachment: MessageAttachment) => {
+        if (this.imageRegex.test(attachment.url)) runFilter(message.author, attachment.url);
+      });
 
-            if (logChannel != null) logChannel.send(embed);
-          }
-        }
+      message.content.match(this.linkRegex)?.forEach((url: string) => {
+        if (this.imageRegex.test(url)) runFilter(message.author, url);
       });
     });
+    async function runFilter(author: User, url: string) {
+      var req = await fetch('https://api.santio.me/filter/image?url=' + url);
+      var data = await req.json();
+      if (data.flags.includes('Porn') || data.flags.includes('Hentai')) {
+        var embed: MessageEmbed = new MessageEmbed()
+          .setTitle(' ')
+          .setColor('#ff0000')
+          .setDescription(`Image was flagged as NSFW [Flags: ${data.flags.join(', ')}]\nPost by: **${author.tag}** *(${author.id})*`)
+          .setImage(url);
+
+        if (logChannel != null) logChannel.send(embed);
+      }
+    }
   }
 }
 
