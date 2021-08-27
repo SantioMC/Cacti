462de7a302647f224a0131547f0b46534ff87f94
Santio <santiothebest123@gmail.com>
2021-08-27 18:57:35 -0400

    Revert changes on commandHandler as I broke everything this is why you dont test on prod :pensive:

--- utils/CommandHandler.ts
+++ utils/CommandHandler.ts
@@ -55,7 +55,7 @@ export class CommandHandler {
     if (message.guild == null) return;
 
     var prefixLength: number = this.client?.data.prefix.length || 0;
-    var parsedArgs: string[] = parseArgs(message.content.replace(/“/g, '"'));
+    var parsedArgs: string[] = parseArgs(message.content);
     var command: string | undefined = parsedArgs.shift()?.toLowerCase().substring(prefixLength);
     if (command == undefined) return;
     var commandObject: Command | undefined = CommandHandler.commands.get(command) || CommandHandler.commands.get(CommandHandler.aliases.get(command) || '');
@@ -294,7 +294,7 @@ export class CommandHandler {
 
 function parseArgs(message: string): string[] {
   message = message.replace(/“/g, '"');
-  var args: string[] = message.match(/[^\s"”']+|"([^"]*)"|'([^']*)'|”([^”]*)”/gi) || [];
+  var args: string[] = message.match(/[^\s"”']+|"([^"]*)"|'([^']*)'/gi) || [];
   args.forEach((arg: string, index: number) => {
     args[index] = arg.substring(1, arg.length - 1);
   });
